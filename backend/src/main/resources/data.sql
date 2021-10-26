06675b4ca107bcc0d4d8392e8c492508e069e436
Thaisa Honorio Morandini <thaisamorandini@gmail.com>
2021-10-26 11:48:56 -0300

    Course

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course(name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML','http://wp.americanacademyofleadership.com/wp-content/uploads/2020/07/courses.jpg','https://upload.wikimedia.org/wikipedia/commons/1/1f/Switch-course-book-grey.svg');
