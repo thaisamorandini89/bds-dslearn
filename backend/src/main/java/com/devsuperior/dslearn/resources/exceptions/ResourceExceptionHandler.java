ee00cb8d9ee8c20a2e298ed471cec2f3a900d47c
Thaisa Honorio Morandini <thaisamorandini@gmail.com>
2022-03-15 21:22:43 -0300

    Validation, Exception Handling

---
+++ backend/src/main/java/com/devsuperior/dslearn/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,63 @@
+package com.devsuperior.dslearn.resources.exceptions;
+
+import java.time.Instant;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.devsuperior.dslearn.services.exceptions.DatabaseException;
+import com.devsuperior.dslearn.services.exceptions.ResourceEntityNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ResourceEntityNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceEntityNotFoundException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err= new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Resource not found");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err= new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Database exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		ValidationError err= new ValidationError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Validation exception");
+		err.setMessage(e.getMessage());
+		
+		List<FieldError> list = e.getBindingResult().getFieldErrors();
+		
+		list.forEach(item -> err.addError(item.getField(), item.getDefaultMessage()));
+		
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+}
--- backend/src/main/java/com/devsuperior/dslearn/services/UserService.java
+++ backend/src/main/java/com/devsuperior/dslearn/services/UserService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dslearn.services;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +9,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dslearn.UserDTO;
 import com.devsuperior.dslearn.entities.User;
 import com.devsuperior.dslearn.repositories.UserRepository;
+import com.devsuperior.dslearn.services.exceptions.ResourceEntityNotFoundException;
 
 
 @Service
@@ -19,6 +24,14 @@ public class UserService implements UserDetailsService  {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Transactional(readOnly = true)
+	public UserDTO findById(Long id) {
+		// TODO Auto-generated method stub
+		Optional<User> obj = userRepository.findById(id);
+		User entity = obj.orElseThrow(() -> new ResourceEntityNotFoundException("Entity not found"));
+		return new UserDTO(entity);
+	}
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
