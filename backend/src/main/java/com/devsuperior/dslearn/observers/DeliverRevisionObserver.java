753aad1fc6efa5bf1a579306c03b9b3f3f51d312
Thaisa Honorio Morandini <thaisamorandini@gmail.com>
2022-03-31 20:29:25 -0300

    Observer design pattern, OCP SOLID principle

---
+++ backend/src/main/java/com/devsuperior/dslearn/observers/DeliverRevisionObserver.java
@@ -0,0 +1,8 @@
+package com.devsuperior.dslearn.observers;
+
+import com.devsuperior.dslearn.entities.Deliver;
+
+public interface DeliverRevisionObserver {
+	
+	void onSaveRevision(Deliver deliver);
+}
--- backend/src/main/java/com/devsuperior/dslearn/services/DeliverService.java
+++ backend/src/main/java/com/devsuperior/dslearn/services/DeliverService.java
@@ -1,5 +1,8 @@
 package com.devsuperior.dslearn.services;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
@@ -7,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dslearn.dto.DeliverRevisionDTO;
 import com.devsuperior.dslearn.entities.Deliver;
+import com.devsuperior.dslearn.observers.DeliverRevisionObserver;
 import com.devsuperior.dslearn.repositories.DeliverRepository;
 
 @Service
@@ -14,6 +18,8 @@ public class DeliverService {
 	
 	@Autowired
 	private DeliverRepository repository;
+	
+	private Set<DeliverRevisionObserver> deliverRevisionObservers = new LinkedHashSet<>();
 	
 	@PreAuthorize("hasAnyRole('ADMIN','INSTRUCTOR')")
 	@Transactional
@@ -23,5 +29,12 @@ public class DeliverService {
 		deliver.setFeedback(dto.getFeedback());
 		deliver.setCorrectCount(dto.getCorrectCount());
 		repository.save(deliver);
+		for(DeliverRevisionObserver observer : deliverRevisionObservers) {
+			observer.onSaveRevision(deliver);
+		}
+	}
+	
+	public void subscribeDeliverRevisionObserver(DeliverRevisionObserver observer) {
+		deliverRevisionObservers.add(observer);
 	}
 }
